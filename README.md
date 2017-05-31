09b501b6c4df26e30519073541bce83f92faeb51
Daniel Wang <ds3wang@uwaterloo.ca>
2017-05-30 22:30:07 -0400

    Add intro activity

---
+++ README.md
@@ -0,0 +1,4 @@
+## Rankr
+Rankr is an Android app that lets you compete in a leaderboard with your friends and co-workers in leagues grouped by organization.
+
+Rankings are established by the Elo Ranking System. 
---
+++ app/src/main/java/ca/danielw/rankr/IntroActivity.java
@@ -0,0 +1,43 @@
+package ca.danielw.rankr;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+public class IntroActivity extends AppIntro {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addSlide(AppIntroFragment.newInstance("rankr", "Join your friends. Climb the ladder.",
+                R.mipmap.ic_launcher, getResources().getColor(R.color.colorPrimary)));
+        addSlide(AppIntroFragment.newInstance("Have fun!", "Elo Ranking System", R.drawable.ic_arrow_back_white,
+                getResources().getColor(R.color.colorPrimary)));
+
+
+        // Hide Skip/Done button.
+        showSkipButton(false);
+        setProgressButtonEnabled(true);
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        // Do something when users tap on Skip button.
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        // Do something when users tap on Done button.
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
+    }
+}
