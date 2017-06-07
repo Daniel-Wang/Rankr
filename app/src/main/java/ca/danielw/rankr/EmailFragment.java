ed59e92b83944470204ff0c84f95b52f6dc4ef78
Daniel Wang <ds3wang@uwaterloo.ca>
2017-06-07 19:56:45 -0400

    Finish email frag and start animations

--- app/src/main/java/ca/danielw/rankr/EmailFragment.java
+++ app/src/main/java/ca/danielw/rankr/EmailFragment.java
@@ -3,14 +3,22 @@ package ca.danielw.rankr;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class EmailFragment extends Fragment {
 
     private static final String TAG = "RootFragment";
+
+    private Button nextBtn;
+    private EditText etEmail;
+    private String email;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,9 +26,46 @@ public class EmailFragment extends Fragment {
 		/* Inflate the layout for this fragment */
         View view = inflater.inflate(R.layout.fragment_email, container, false);
 
+        nextBtn = (Button) view.findViewById(R.id.btnNext);
+        etEmail = (EditText) view.findViewById(R.id.etEmail);
 
+        etEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                email = etEmail.getText().toString();
+                if(isValidEmail(email)){
+                    etEmail.setEnabled(false);
+                }
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.root_frame, new LeagueNameFragment());
+            }
+        });
 
         return view;
+    }
+
+    public final static boolean isValidEmail(CharSequence target) {
+        if (target == null) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
     }
 
 }
