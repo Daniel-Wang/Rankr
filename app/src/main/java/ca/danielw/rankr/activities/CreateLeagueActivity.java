60586b693b5834964ca7e1afa5449d6e47d5a4c8
Daniel Wang <ds3wang@uwaterloo.ca>
2017-06-10 19:15:50 -0400

    Add firebase create user

--- app/src/main/java/ca/danielw/rankr/activities/CreateLeagueActivity.java
+++ app/src/main/java/ca/danielw/rankr/activities/CreateLeagueActivity.java
@@ -1,11 +1,18 @@
 package ca.danielw.rankr.activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import ca.danielw.rankr.R;
 import ca.danielw.rankr.adapters.SlidePagerAdapter;
@@ -13,8 +20,9 @@ import ca.danielw.rankr.adapters.SlidePagerAdapter;
 public class CreateLeagueActivity extends AppCompatActivity {
 
     public static final String SOURCE_LOCATION = "CREATE_LEAGUE";
+    public static final String TAG = CreateLeagueActivity.class.getName();
 
-    private FirebaseAuth mAuth;
+    public static FirebaseAuth mAuth;
 
     private ViewPager mPager;
     private SlidePagerAdapter mPagerAdapter;
@@ -35,5 +43,6 @@ public class CreateLeagueActivity extends AppCompatActivity {
         mPager = (ViewPager) findViewById(R.id.vpPager);
         mPagerAdapter = new SlidePagerAdapter(getSupportFragmentManager(), SOURCE_LOCATION);
         mPager.setAdapter(mPagerAdapter);
+
     }
 }
--- app/src/main/java/ca/danielw/rankr/fragments/PasswordFragment.java
+++ app/src/main/java/ca/danielw/rankr/fragments/PasswordFragment.java
@@ -1,15 +1,23 @@
 package ca.danielw.rankr.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
 
 import ca.danielw.rankr.R;
 import ca.danielw.rankr.activities.CreateLeagueActivity;
@@ -36,7 +44,7 @@ public class PasswordFragment extends Fragment {
                 CreateLeagueActivity.mPassword = password;
 
                 //Start the leaderboard activity
-
+                createUser();
             }
         });
 
@@ -60,5 +68,25 @@ public class PasswordFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void createUser(){
+
+        CreateLeagueActivity.mAuth.createUserWithEmailAndPassword(CreateLeagueActivity.mEmail, CreateLeagueActivity.mPassword)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = CreateLeagueActivity.mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
