bc6cac201a71f724cd8f1c60ed42b99d9d478029
Daniel Wang <ds3wang@uwaterloo.ca>
2017-07-06 23:29:51 -0400

    Add image icons for Game result

---
+++ app/src/main/java/ca/danielw/rankr/activities/EnterGameResultActivity.java
@@ -0,0 +1,17 @@
+package ca.danielw.rankr.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.danielw.rankr.R;
+
+public class EnterGameResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_game_result);
+
+
+    }
+}
